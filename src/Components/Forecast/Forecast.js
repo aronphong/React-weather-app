c1af146239f88a3a5d9e7a007e7b710adff4c091
aron <aronphong@gmail.com>
2020-03-26 10:35:31 -0700

    Add units

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -3,17 +3,22 @@ import styles from './Forecast.module.css';
 
 // Display data
 const Forecast = (props) => {
-   let data = props.data;
+  let data = props.data;
+  let units = props.units === "imperial" ? "\u00b0F" : "\u00b0C";
   return (
     <div className={styles.Forecast}>
-        <div>{data.temp} {data.main}</div>
+        <div className={styles.name}>{data.name}</div>
+        <div>{data.temp}{units} - {data.main}</div>
         <div>{data.description} throughout the day</div>
-        <div>Feels Like - {data.feelsLike}</div>
-        <div>Low - {data.tempMin}</div>
-        <div>High - {data.tempMax}</div>
-        <div>Pressure - {data.pressure}</div>
+        <div>
+          <p>Feels Like - {data.feelsLike}{units}</p>
+          <p>Low - {data.tempMin}{units}</p>
+          <p>High - {data.tempMax}{units}</p>          
+        </div>
+
+        {/* <div>Pressure - {data.pressure}</div> */}
         <div>Wind Speed - {data.wind.speed}</div>
-        <div>Wind Direction - {data.wind.degrees} degrees</div>
+        <div>Wind Direction - {data.wind.degrees}{'\u00b0'}</div>
     </div>
   );
 };
