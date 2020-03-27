dd1818a76a6003184efda359ffbc554aa912133c
aron <aronphong@gmail.com>
2020-03-27 06:59:53 -0700

    Refactor Form into function component

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import styles from './Form.module.css';
 
-class Form extends Component {
+const Form = (props) => {
 
-    getInput = () => {
+    const getInput = () => {
         let userInput = document.getElementsByTagName('input')[0].value;
-        this.props.getLocation(userInput);
-    }
-
-    handleSearch = () => {
-        if (this.props.location === "") {
-            document.getElementsByTagName('input')[0].focus();
-            return;
-        }
-        this.props.searchWeather();
+        props.getLocation(userInput);
     };
 
-    render () {
-        return (
-            <div className={styles.Form}>
-                <input
-                    autoFocus 
-                    className={styles.search} 
-                    type="text"
-                    maxLength="35" 
-                    onChange={this.getInput}
-                    value={this.props.location}
-                    placeholder="Enter City name"  />
-                <button onClick={this.props.switchUnits}>{this.props.units}</button>
-                <button onClick={this.handleSearch}>Search</button>
-            </div>
-        );
-    }
+    const handleSearch = () => {
+        if (props.location === "") {
+            document.getElementsByTagName('input')[0].focus();
+            return;
+        };
+        props.searchWeather();
+    };
+
+    return (
+        <div className={styles.Form}>
+            <input
+                autoFocus 
+                className={styles.search} 
+                type="text"
+                maxLength="35" 
+                onChange={getInput}
+                value={props.location}
+                placeholder="Enter City name"  
+            />
+        <button onClick={props.switchUnits}>{props.units}</button>
+        <button onClick={handleSearch}>Search</button>
+        </div>
+    );
 };
 
 export default Form;
